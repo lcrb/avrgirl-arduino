8e5401ad07366bb314d67fcfabe32c76d5634f64
William Fligor <will@liveify.com>
2016-03-07 11:56:35 -0500

    Reindent

--- avrgirl-arduino.js
+++ avrgirl-arduino.js
@@ -8,29 +8,29 @@ var protocols = require('./lib/protocols');
  * @param {object} opts - options for consumer to pass in
  */
 var AvrgirlArduino = function(opts) {
-  opts = opts || {};
+    opts = opts || {};
 
-  this.options = {
-    debug: opts.debug || false,
-    board: opts.board || 'uno',
-    port: opts.port || ''
-  };
+    this.options = {
+        debug: opts.debug || false,
+        board: opts.board || 'uno',
+        port: opts.port || ''
+    };
 
-  this.debug = this.options.debug ? console.log.bind(console) : function() {};
+    this.debug = this.options.debug ? console.log.bind(console) : function() {};
 
-  this.connection = new Connection(this.options);
+    this.connection = new Connection(this.options);
 
-  this.board = boards.byName[this.options.board];
+    this.board = boards.byName[this.options.board];
 
-  if (this.board) {
-    var Protocol = protocols[this.board.protocol] || function() {};
+    if (this.board) {
+        var Protocol = protocols[this.board.protocol] || function() {};
 
-    this.protocol = new Protocol({
-      board: this.board,
-      connection: this.connection,
-      debug: this.options.debug
-    });
-  }
+        this.protocol = new Protocol({
+            board: this.board,
+            connection: this.connection,
+            debug: this.options.debug
+        });
+    }
 };
 
 /**
@@ -39,22 +39,22 @@ var AvrgirlArduino = function(opts) {
  * @param {function} callback - function to run upon completion/error
  */
 AvrgirlArduino.prototype._validateBoard = function(callback) {
-  if (!this.board) {
-    // cannot find a matching board in supported list
-    return callback(new Error('"' + this.options.board + '" is not a supported board type.'));
+    if (!this.board) {
+        // cannot find a matching board in supported list
+        return callback(new Error('"' + this.options.board + '" is not a supported board type.'));
 
-  } else if (!this.protocol.chip) {
-    // something went wrong trying to set up the protocol
-    return callback(new Error('not a supported programming protocol: ' + this.board.protocol));
+    } else if (!this.protocol.chip) {
+        // something went wrong trying to set up the protocol
+        return callback(new Error('not a supported programming protocol: ' + this.board.protocol));
 
-  } else if (!this.options.port && this.options.board === 'pro-mini') {
-    // when using a pro mini, a port is required in the options
-    return callback(new Error('using a pro-mini, please specify the port in your options.'));
+    } else if (!this.options.port && this.options.board === 'pro-mini') {
+        // when using a pro mini, a port is required in the options
+        return callback(new Error('using a pro-mini, please specify the port in your options.'));
 
-  } else {
-    // all good
-    return callback(null);
-  }
+    } else {
+        // all good
+        return callback(null);
+    }
 };
 
 /**
@@ -64,20 +64,20 @@ AvrgirlArduino.prototype._validateBoard = function(callback) {
  * @param {function} callback - function to run upon completion/error
  */
 AvrgirlArduino.prototype.flash = function(file, callback) {
-  var _this = this;
+    var _this = this;
 
-  // validate board properties first
-  _this._validateBoard(function(error) {
-    if (error) { return callback(error); }
+    // validate board properties first
+    _this._validateBoard(function(error) {
+        if (error) { return callback(error); }
 
-    // set up serialport connection
-    _this.connection._init(function(error) {
-      if (error) { return callback(error); }
+        // set up serialport connection
+        _this.connection._init(function(error) {
+            if (error) { return callback(error); }
 
-      // upload file to board
-      _this.protocol._upload(file, callback);
+            // upload file to board
+            _this.protocol._upload(file, callback);
+        });
     });
-  });
 };
 
 /**
@@ -87,8 +87,8 @@ AvrgirlArduino.prototype.flash = function(file, callback) {
  * @param {function} callback - function to run upon completion/error
  */
 AvrgirlArduino.prototype.listPorts = AvrgirlArduino.listPorts =
-AvrgirlArduino.prototype.list = AvrgirlArduino.list = function(callback) {
-  return Connection.prototype._listPorts(callback);
+    AvrgirlArduino.prototype.list = AvrgirlArduino.list = function(callback) {
+    return Connection.prototype._listPorts(callback);
 };
 
 module.exports = AvrgirlArduino;
